49deaf54e3d4a923e92d4b88aec9085a32c6ca9b
Philip Andersson <philipodev@gmail.com>
2025-11-05 20:38:59 +0100

    https support

--- src/proxy/proxy-client.ts
+++ src/proxy/proxy-client.ts
@@ -13,49 +13,47 @@ export class ProxyClient implements IProxyClient {
 		this.rejectUnauthorized = rejectUnauthorized;
 	}
 
-	async forward(targetUrl: string, requestInfo: RequestInfo): Promise<ResponseInfo> {
+	async forward(
+		targetUrl: string,
+		requestInfo: RequestInfo,
+	): Promise<ResponseInfo> {
 		return new Promise<ResponseInfo>((resolve, reject) => {
 			const proxyUrl = new URL(requestInfo.url, targetUrl);
 			const isHttps = proxyUrl.protocol === "https:";
 			const request = isHttps ? httpsRequest : httpRequest;
-			
+
 			const headers = { ...requestInfo.headers };
 			headers.host = proxyUrl.host;
-			
+
 			const requestOptions = {
 				method: requestInfo.method,
 				headers,
-				...(isHttps && { 
+				...(isHttps && {
 					rejectUnauthorized: this.rejectUnauthorized,
 					servername: proxyUrl.hostname,
 				}),
 			};
-			
-			const proxyReq = request(
-				proxyUrl,
-				requestOptions,
-				async (proxyRes) => {
-					try {
-						const body = await this.requestHandler.readBody(proxyRes);
-						
-						resolve({
-							statusCode: proxyRes.statusCode ?? 200,
-							headers: proxyRes.headers,
-							body,
-						});
-					} catch (err) {
-						reject(err);
-					}
-				},
-			);
-			
+
+			const proxyReq = request(proxyUrl, requestOptions, async (proxyRes) => {
+				try {
+					const body = await this.requestHandler.readBody(proxyRes);
+
+					resolve({
+						statusCode: proxyRes.statusCode ?? 200,
+						headers: proxyRes.headers,
+						body,
+					});
+				} catch (err) {
+					reject(err);
+				}
+			});
+
 			proxyReq.on("error", (err) => {
 				reject(err);
 			});
-			
+
 			proxyReq.write(requestInfo.body);
 			proxyReq.end();
 		});
 	}
 }
-
